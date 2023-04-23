90c66bbfc406fa2c0271b87e207fcae1d7965a1e
Neil Gowran <neil@wpbeaches.com>
2023-04-23 14:32:46 +1000

    Update cache-purge-helper.php

--- cache-purge-helper.php
+++ cache-purge-helper.php
@@ -27,35 +27,35 @@
 * is activated for purging server cache.
 */
 
-function cphp_purge() {
+function rclc_purge() {
     // Purge WordPress Cache
     $called_action_hook = current_filter();
-    cphp_write_log('cphp - Initiated');
-    cphp_write_log('cphp - Running on '. $called_action_hook );
-    cphp_write_log('cphp - Flushing WordPress Cache first');
+    rclc_write_log('rclc - Initiated');
+    rclc_write_log('rclc - Running on '. $called_action_hook );
+    rclc_write_log('rclc - Flushing WordPress Cache first');
     wp_cache_flush();
  
     // If litespeed-cache plugins is enabled, purge cache.
-    cphp_write_log('cphp - Checking for litespeed-cache plugin');
+    rclc_write_log('rclc - Checking for litespeed-cache plugin');
 
     if ( is_plugin_active('litespeed-cache/litespeed-cache.php') ) {
-        cphp_write_log('cphp - Litespeed-cache plugin installed, running do_action(\'litespeed_purge_all\');');
+        rclc_write_log('rclc - Litespeed-cache plugin installed, running do_action(\'litespeed_purge_all\');');
         do_action( 'litespeed_purge_all' );
     }  else {
-        cphp_write_log('cphp - Litespeed-cache plugin not installed or detected');
+        rclc_write_log('rclc - Litespeed-cache plugin not installed or detected');
     }
 
     // End of cache_purge_helper()
-    cphp_write_log('cphp - End of cache_purge_helper function');
+    rclc_write_log('rclc - End of cache_purge_helper function');
 }
 
   // If RunCloud plugins is enabled, purge cache.
-  cphp_write_log('cphp - Checking for RunCloud plugin');
+  rclc_write_log('rclc - Checking for RunCloud plugin');
 	if ( class_exists('RunCloud_Hub') && is_callable( [ 'RunCloud_Hub', 'purge_cache_all_noaction' ] ) ) {
 	    RunCloud_Hub::purge_cache_all_noaction();
-	    cphp_write_log('cphp - Flushing RunCloud cache out');
+	    rclc_write_log('rclc - Flushing RunCloud cache out');
 	} else {
-    cphp_write_log('cphp - No RunCloud Plugin here');
+    rclc_write_log('rclc - No RunCloud Plugin here');
   }
 
 /** 
@@ -63,7 +63,7 @@ function cphp_purge() {
  * Log to PHP error_log if WP_DEBUG and CPH_DEBUG are set!
  */
 
-function cphp_write_log ( $log )  {
+function rclc_write_log ( $log )  {
     if ( WP_DEBUG === true  ) {
         if ( is_array( $log ) || is_object( $log ) ) {
             error_log( print_r( $log, true ) );
@@ -79,11 +79,11 @@ function cphp_write_log ( $log )  {
  * WordPress core hooks.
  */
 
-cphp_write_log('cphp - Loading WordPress core hooks');
-add_action( 'upgrader_process_complete', 'cphp_purge', 10, 0 ); // After a plugin, theme or core has been updated.
-add_action( 'activated_plugin', 'cphp_purge', 10, 0); // After a plugin has been activated
-add_action( 'deactivated_plugin', 'cphp_purge', 10, 0); // After a plugin has been deactivated
-add_action( 'switch_theme', 'cphp_purge', 10, 0); // After a theme has been changed
+rclc_write_log('rclc - Loading WordPress core hooks');
+add_action( 'upgrader_process_complete', 'rclc_purge', 10, 0 ); // After a plugin, theme or core has been updated.
+add_action( 'activated_plugin', 'rclc_purge', 10, 0); // After a plugin has been activated
+add_action( 'deactivated_plugin', 'rclc_purge', 10, 0); // After a plugin has been deactivated
+add_action( 'switch_theme', 'rclc_purge', 10, 0); // After a theme has been changed
 
 /**
  * Page builder hooks.
@@ -91,26 +91,26 @@ add_action( 'switch_theme', 'cphp_purge', 10, 0); // After a theme has been chan
 
 // Beaver Builder
 if ( defined( 'FL_BUILDER_VERSION' ) ) {
-    cphp_write_log('cphp - Beaver Builder Hooks enabled');
-    add_action( 'fl_builder_cache_cleared', 'cphp_purge', 10, 3 );
-    add_action( 'fl_builder_after_save_layout', 'cphp_purge', 10, 3 );
-    add_action( 'fl_builder_after_save_user_template', 'cphp_purge', 10, 3 );
+    rclc_write_log('rclc - Beaver Builder Hooks enabled');
+    add_action( 'fl_builder_cache_cleared', 'rclc_purge', 10, 3 );
+    add_action( 'fl_builder_after_save_layout', 'rclc_purge', 10, 3 );
+    add_action( 'fl_builder_after_save_user_template', 'rclc_purge', 10, 3 );
 }
 
 // Elementor
 if ( defined( 'ELEMENTOR_VERSION' ) ) {
-    cphp_write_log('cphp - Elementor hooks enabled');
-    add_action( 'elementor/core/files/clear_cache', 'cphp_purge', 10, 3 ); 
-    add_action( 'update_option__elementor_global_css', 'cphp_purge', 10, 3 );
-    add_action( 'delete_option__elementor_global_css', 'cphp_purge', 10, 3 );
+    rclc_write_log('rclc - Elementor hooks enabled');
+    add_action( 'elementor/core/files/clear_cache', 'rclc_purge', 10, 3 ); 
+    add_action( 'update_option__elementor_global_css', 'rclc_purge', 10, 3 );
+    add_action( 'delete_option__elementor_global_css', 'rclc_purge', 10, 3 );
 }
 
 // Oxygen
 if ( defined( 'CT_VERSION' ) ) {
-    cphp_write_log('cphp - Oxygen hooks enabled');
-    add_action( 'wp_ajax_oxygen_vsb_cache_generated','cphp_purge', 99 );
-    add_action( 'update_option__oxygen_vsb_universal_css_url','cphp_purge', 99 );
-    add_action( 'update_option__oxygen_vsb_css_files_state','cphp_purge', 99 );
+    rclc_write_log('rclc - Oxygen hooks enabled');
+    add_action( 'wp_ajax_oxygen_vsb_cache_generated','rclc_purge', 99 );
+    add_action( 'update_option__oxygen_vsb_universal_css_url','rclc_purge', 99 );
+    add_action( 'update_option__oxygen_vsb_css_files_state','rclc_purge', 99 );
 }
 
 /**
@@ -119,12 +119,12 @@ if ( defined( 'CT_VERSION' ) ) {
 
 // Autoptimizer
 if ( defined( 'AUTOPTIMIZE_PLUGIN_DIR' ) ) {
-    cphp_write_log('cphp - Autoptimize hooks enabled');
-    add_action( 'autoptimize_action_cachepurged','cphp_purge', 10, 3 ); // Need to document this.
+    rclc_write_log('rclc - Autoptimize hooks enabled');
+    add_action( 'autoptimize_action_cachepurged','rclc_purge', 10, 3 ); // Need to document this.
 }
 
 // WP Optimize Hooks
 if ( defined ('WPO_VERSION') ){
-    cphp_write_log('cphp - WP Optimize hooks enabled');
+    rclc_write_log('rclc - WP Optimize hooks enabled');
     add_filter('wpo_purge_all_cache_on_update', '__return_true');
 }
